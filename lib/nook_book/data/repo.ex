bc51a7d7edf26ebe7fa26a4b8b6db71fc0cb1b49
Chris Hildebrand <chris.hildebrand@versussystems.com>
2020-08-28 13:14:19 -0400

    Step 6

--- lib/nook_book/data/repo.ex
+++ lib/nook_book/data/repo.ex
@@ -23,7 +23,7 @@ defmodule NookBook.Data.Repo do
 
   def filter(table, pattern) do
     :mnesia.transaction(fn ->
-      :mnesia.match_object(table, :mnesia.table_info(table, pattern), :read)
+      :mnesia.match_object(table, pattern, :read)
     end)
   end
 end
