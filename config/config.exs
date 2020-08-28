ef730156ab5bef6be53e4fa21b8124e5313a217b
Chris Hildebrand <chris.hildebrand@versussystems.com>
2020-08-28 17:23:50 -0400

    Step 21, final build

--- config/config.exs
+++ config/config.exs
@@ -15,6 +15,8 @@ config :nook_book, NookBookWeb.Endpoint,
   pubsub_server: NookBook.PubSub,
   live_view: [signing_salt: "NUyDg1li"]
 
+config :libcluster, :topologies, []
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
--- config/releases.exs
+++ config/releases.exs
@@ -1,7 +1,22 @@
 import Config
 
+config :libcluster,
+  topologies: [
+    nook_book: [
+      strategy: Cluster.Strategy.Epmd,
+      config: [
+        hosts: [
+          :"nook_book@10.0.1.102",
+          :"nook_book@10.0.1.243"
+        ]
+      ]
+    ]
+  ]
+
+config :nook_book, cluster_role: System.get_env("CLUSTER_ROLE", "member") |> String.to_atom()
+
 config :nook_book, NookBookWeb.Endpoint,
   server: true,
   http: [port: 4000],
-  url: [host: "localhost"],
+  url: [host: "follow.nookbook.online"],
   secret_key_base: "P302LUMxFCBc2NvN15qXnupU8vqrvGzWMFDRjlvpxpTGwTlcx0d00i8kgJHkLAWM"
